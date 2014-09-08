429fe02739e705c949f2495ecbc1a10784317cbb
David Cramer <david@digilab.co.za>
2014-09-08 16:15:05 +0200

    examples

--- examples/baldrick-examples.php
+++ examples/baldrick-examples.php
@@ -8,7 +8,7 @@
 	data-modal-width="500px"
 	data-modal-height="500px"
 	data-modal-center="true"
-	data-modal-buttons="Close|dismiss"
+	data-modal-buttons='Close|dismiss;Save|{"data-target" : "#text-area-src", "data-modal-close": true, "data-request" : "#example-modal_baldrickModal"}'
 
 >Modal Request (500x500 centered)</button>
 
@@ -81,7 +81,26 @@
 	data-modal-buttons="Close|dismiss;Save|alert|button-primary"
 
 >Modal Request (Template)</button>
-<textarea id="text-area-src">[{"key":"modal","value":"example-modal"},{"key":"modalTitle","value":"Baldrick Modal Example"},{"key":"modalButtons","value":"Close|dismiss;Save|alert|button-primary"},{"key":"modalWidth","value":"500px"},{"key":"modalHeight","value":"500px"},{"key":"action","value":"baldrick_examples"},{"key":"template","value":"#modal_content_table_example"},{"key":"modalCenter","value":"true"}]</textarea>
+<button class="button wp-ajax" type="button" 
+	
+	data-action="baldrick_examples_json"
+	data-target="#text-area-src"
+
+>Request to textarea</button>
+<textarea style="width:100%;" id="text-area-src" class="wp-ajax" data-event="change" data-modal="textarea" data-modal-title="this data" data-template="#modal_content_table_example">[{"key":"modal","value":"example-modal"},{"key":"modalTitle","value":"Baldrick Modal Example"},{"key":"modalButtons","value":"Close|dismiss;Save|alert|button-primary"},{"key":"modalWidth","value":"500px"},{"key":"modalHeight","value":"500px"},{"key":"action","value":"baldrick_examples"},{"key":"template","value":"#modal_content_table_example"},{"key":"modalCenter","value":"true"}]</textarea>
+
+<hr>
+<form id="test_form">
+<input type="text" name="username" value="username"><br>
+<input type="text" name="password" value="password"><br>
+<input type="text" name="email" value="email"><br>
+<input type="text" name="data[add]" value="data[add]"><br>
+<input type="text" name="data[more]" value="data[more]"><br>
+<input type="text" name="data[fields]" value="data[fields]"><br>
+<button type="button" class="wp-ajax" data-request="#test_form" data-template="#form_template" data-modal="woooter">Send</button>
+</form>
+<hr>
+<div id="form_template_resault"></div>
 <script type="text/html" id="modal_content_example">
 <button class="button wp-ajax" type="button" 
 	
@@ -95,9 +114,16 @@
 	data-request="#modal_content_example"
 
 >Modal Request (500x500 centered)</button>
-	
-</script>
+<hr>
 
+
+</script>
+<script id="form_template" type="text/html">
+<h2>{{username}}</h2>
+	{{#each data}}
+	<p>{{this}}</p>
+	{{/each}}
+</script>
 
 <script type="text/html" id="modal_content_table_example">
 <table class="widefat">
@@ -112,5 +138,14 @@
 	</tr>
 	{{/each}}
 </table>	
+</script>
+
+<script type="text/html" id="modal_content_form">
+<input type="text" name="username" value="username"><br>
+<input type="text" name="password" value="password"><br>
+<input type="text" name="email" value="email"><br>
+<input type="text" name="data[add]" value="items"><br>
+<input type="text" name="data[more]" value="more"><br>
+<input type="text" name="data[fields]" value="stuff"><br>
 </script>
 
